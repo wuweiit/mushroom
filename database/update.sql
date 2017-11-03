8dada2d990c2fff40a19117023bc0f6f59a4a250
wuw <Leleyun1029>
2017-11-03 22:39:51 +0800

    1. [后台]  优化荣誉管理功能

--- database/update.sql
+++ database/update.sql
@@ -3,3 +3,7 @@
 
 ALTER TABLE `mr_project` ADD COLUMN `stick` int AFTER `extJson`;
 ALTER TABLE `mr_article` ADD COLUMN `stick` int AFTER `updateTime`;
+
+
+
+ALTER TABLE `mr_honer` CHANGE COLUMN `icon` `icon` text DEFAULT NULL;
