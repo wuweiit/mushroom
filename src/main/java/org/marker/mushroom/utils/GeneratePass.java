f32f28a22f0fd7c9a92e1b0c76724b29e2d361f0
wuwei <wuwei@byitgroup.com>
2018-06-03 13:19:35 +0800

    处理问题

--- src/main/java/org/marker/mushroom/utils/GeneratePass.java
+++ src/main/java/org/marker/mushroom/utils/GeneratePass.java
@@ -1,7 +1,5 @@
 package org.marker.mushroom.utils;
 
-import org.marker.mushroom.core.config.impl.SystemConfig;
-import org.marker.mushroom.holder.SpringContextHolder;
 import org.marker.security.Base64;
 import org.marker.security.DES;
 import org.marker.security.MD5;
@@ -15,8 +13,7 @@ public class GeneratePass {
 
 	
 	public static String encode(String password) {
-		SystemConfig syscfg = SystemConfig.getInstance();
-		String key = syscfg.get("secret_key");
+		String key = "iSDCRbyRZP0=";
 		try {
 			return MD5.getMD5Code(Base64.encode(DES.encrypt(
                        password.getBytes(), key)));
