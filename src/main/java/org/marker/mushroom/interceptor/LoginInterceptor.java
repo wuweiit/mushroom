c12c1966b377b16d3640506a6d3b78cef545972e
wuwei <wuwei@byitgroup.com>
2018-08-11 18:31:51 +0800

    优化

--- src/main/java/org/marker/mushroom/interceptor/LoginInterceptor.java
+++ src/main/java/org/marker/mushroom/interceptor/LoginInterceptor.java
@@ -1,17 +1,13 @@
 package org.marker.mushroom.interceptor;
 
-import java.io.PrintWriter;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import org.marker.mushroom.core.AppStatic;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import org.marker.mushroom.core.AppStatic;
-import org.marker.mushroom.core.config.impl.SystemConfig;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
+import java.io.PrintWriter;
 
 
 
@@ -40,10 +36,11 @@ public class LoginInterceptor implements HandlerInterceptor  {
 			
 			if(accept.matches(".*application/json.*")){// json数据请求 
 				out.write("{\"status\":false,\"code\":\"101\",\"message\":\"当前会话失效，请重新登录系统!\"}");
+			} else {
+				// HTML页面
+				out.write("<script type='text/javascript'>window.location.href='login.do?status=timeout';</script>");
 			}
-			
-			// HTML页面
-			out.write("<script type='text/javascript'>window.location.href='login.do?status=timeout';</script>");
+
 			out.flush();
 			out.close();
 			return false;
