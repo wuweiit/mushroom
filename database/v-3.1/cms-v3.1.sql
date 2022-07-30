2e89eba45f3a4c958c01c0dbc0116a769c856de6
marker <admin@wuweibi.com>
2022-07-30 11:53:35 +0800

    优化留言功能

--- database/v-3.1/cms-v3.1.sql
+++ database/v-3.1/cms-v3.1.sql
@@ -3,3 +3,8 @@
 ALTER TABLE `mr_sys_language`
     ADD INDEX `idx_key`(`key`);
 
+-- 2022-07-30 留言增加邮箱地址
+ALTER TABLE `mr_guestbook`
+    ADD COLUMN `email` varchar(200) NULL COMMENT '邮箱地址' AFTER `id`;
+ALTER TABLE `mr_guestbook`
+    MODIFY COLUMN `status` int(1) UNSIGNED NULL DEFAULT NULL COMMENT '状态 0待审核 1已审核' AFTER `time`;
